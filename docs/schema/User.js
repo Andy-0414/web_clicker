ea490746c9b5358541d1e51ca72d6d57e2267ba2
Andy0414 <pjh8667@naver.com>
2019-03-14 18:24:34 +0900

    UPDATE schema

--- docs/schema/User.js
+++ docs/schema/User.js
@@ -4,5 +4,7 @@ var Schema = mongoose.Schema;
 module.exports = mongoose.model('userData', new Schema({
     nickname : String,
     password : String,
-    
+    level : Number,
+    xp : Number,
+    money : Number
 }));
