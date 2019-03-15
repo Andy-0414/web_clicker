ea26f43643e90ac2c14b8e3f046fb41479a4ef11
Andy0414 <pjh8667@naver.com>
2019-03-15 19:51:32 +0900

    UPDATE style

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 
 const MONGO = require('./modules/mongoConnectionModule')
 
-app.listen(3000,()=>{
+app.use(express.static('./docs'))
 
+app.listen(3000,()=>{
+    console.log("Hello World!")
 })
 
 app.get('/',(req,res)=>{
     res.sendfile('./docs/index.html')
 })
+
